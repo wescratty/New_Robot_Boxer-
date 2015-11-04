60f7b407df3ca00b338bc7ada3b145757e29e81d
Brian_Trethewey <brian.trethewey@umontana.edu>
2015-11-04 14:30:29 -0700

    BoxerBuilder stub created

---
+++ src/BlockBuilder.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+/**
+ * Created by Brian Trethewey on 11/4/15.
+ */
+//todo this class
+public class BlockBuilder {
+
+private ArrayList<Block> blockList;
+
+    public ArrayList<Block>buildBlocks(int unusedPoints){
+        return blockList;
+    }
+}
