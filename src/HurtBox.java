7d61055529df778bfa82b46076a788553a660aee
Brian_Trethewey <brian.trethewey@umontana.edu>
2015-11-04 14:47:11 -0700

    Added HurtBox

---
+++ src/HurtBox.java
@@ -0,0 +1,15 @@
+/**
+ * Created by Brian Trethewey on 11/4/15.
+ */
+
+//todo this class
+public class HurtBox {
+
+    private DamageCalculation heavyCalculation;
+    private DamageCalculation fastAttack;
+
+    public String calculateDamage(Attack attack, Block block){
+        return "Not Implimented";
+    }
+
+}
