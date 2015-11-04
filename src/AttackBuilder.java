c309e4a0b0ad9a8ffed7acbc4a77dc29c76e4dc0
Brian_Trethewey <brian.trethewey@umontana.edu>
2015-11-04 14:26:55 -0700

    AttackBuilder stub created

---
+++ src/AttackBuilder.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+
+/**
+ * Created by Brian Trethewey on 11/4/15.
+ */
+//todo this class
+public class AttackBuilder {
+    private ArrayList<Attack> attackList;
+
+    private ArrayList<Attack> buildBasicAttacks(){
+        return null;
+    }
+
+    private Attack buildSimpleAttack(String inputs){
+        return null;
+    }
+
+    public ArrayList<Attack>buildAttacks(int unusedPoints){
+        return attackList;
+    }
+
+    public ArrayList<Attack>buildAIAttacks(int unusedPoints){
+        return attackList;
+    }
+}
+
