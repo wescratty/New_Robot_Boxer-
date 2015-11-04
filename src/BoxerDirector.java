87b5033f3937f8f01231e8adb925bd41a0355165
Brian_Trethewey <brian.trethewey@umontana.edu>
2015-11-04 14:34:39 -0700

    BoxerDirector  stub created

---
+++ src/BoxerDirector.java
@@ -0,0 +1,21 @@
+/**
+ * Created by Brian Trethewey on 11/4/15.
+ */
+//todo this class
+public class BoxerDirector {
+
+    private BoxerBuilder boxerbuilder;
+    private AttackBuilder attackBuilder;
+    private BlockBuilder blockBuilder;
+    private Boxer boxer;
+    int points;
+    int unusedPoints;
+
+    public Boxer build(){
+        return boxer;
+    }
+
+    public Boxer buildAI(){
+        return boxer;
+    }
+}
