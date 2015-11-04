5dffe4f29f563a7835bd30cb40c93b8744b6d99f
Brian_Trethewey <brian.trethewey@umontana.edu>
2015-11-04 14:18:39 -0700

    Boxer builder stub created

---
+++ src/BoxerBuilder.java
@@ -0,0 +1,20 @@
+/**
+ * Created by sinless on 11/4/15.
+ */
+public class BoxerBuilder {
+    private Boxer boxer;
+    private RNG rng;
+    private Boxer setStats(int unusedPoints){
+        boxer = new Boxer();
+        //set call dialogue box
+
+        return boxer;
+    }
+
+    private Boxer setAIStats(int unusedPoints){
+        boxer = new Boxer();
+        //set random stats
+
+        return boxer;
+    }
+}
