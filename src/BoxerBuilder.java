60bfb757742eccaea8f15c7d0ee732485ad9904c
Brian_Trethewey <brian.trethewey@umontana.edu>
2015-11-05 01:21:58 -0700

    added RNG to builder

--- src/BoxerBuilder.java
+++ src/BoxerBuilder.java
@@ -4,10 +4,15 @@
 public class BoxerBuilder {
     private Boxer boxer;
     private RNG rng;
+
+    public BoxerBuilder() {
+        rng = RNG.getInstance();
+    }
+
     private Boxer setStats(int unusedPoints){
         boxer = new Boxer();
-        //set call dialogue box
-
+        //call dialogue box
+        // set user specified stats
         return boxer;
     }
 
