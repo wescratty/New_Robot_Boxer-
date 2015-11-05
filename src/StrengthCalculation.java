1d71377825f2203ff8d21e8faf03887a7c37586c
Brian_Trethewey <brian.trethewey@umontana.edu>
2015-11-05 13:04:05 -0700

    added COR modules

---
+++ src/StrengthCalculation.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Brian Trethewey on 11/5/15.
+ */
+//todo this class
+public class StrengthCalculation extends DamageCalculation {
+    @Override
+    public String execute(Attack attack, Block block, int damage) {
+        return null;
+    }
+}
