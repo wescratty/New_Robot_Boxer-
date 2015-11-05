0323f7eb32e40e18a5ea6614de433bcb711ffb73
Brian_Trethewey <brian.trethewey@umontana.edu>
2015-11-05 14:49:43 -0700

    first pass boxer builder

--- src/BoxerBuilder.java
+++ src/BoxerBuilder.java
@@ -4,22 +4,55 @@
 public class BoxerBuilder {
     private Boxer boxer;
     private RNG rng;
+    private int pointsUsed;
 
     public BoxerBuilder() {
         rng = RNG.getInstance();
     }
 
-    private Boxer setStats(int unusedPoints){
+    /**
+     * Function for the creation of a boxer this includes the initial pass for stat allocation
+     * @param unusedPoints number of points the player has available to assign to stats
+     * @return A boxer object with the stats supplied by the player
+     */
+    public Boxer createBoxer(int unusedPoints){
         boxer = new Boxer();
+        pointsUsed =  setStats(unusedPoints);
+        return boxer;
+    }
+    /**
+     * Function for the autonomous creation of a boxer this includes the initial pass for stat allocation
+     * @param unusedPoints number of points the computer has available to assign to stats
+     * @return A boxer object with the stats created via heuristic
+     */
+    public Boxer createAIBoxer(int unusedPoints){
+        boxer = new Boxer();
+        setAIStats(unusedPoints);
+        return boxer;
+    }
+
+    /**
+     * Method to handle the allocation of stats to the boxer
+     * @param unusedPoints number of points the player has available to assign to stats
+     * @return number of points spent during this step
+     */
+    private int setStats(int unusedPoints){
+        int pointsSpent = 0;
         //call dialogue box
-        // set user specified stats
-        return boxer;
+        //set boxer stats
+        return pointsSpent;
     }
 
-    private Boxer setAIStats(int unusedPoints){
-        boxer = new Boxer();
-        //set random stats
-
-        return boxer;
+    /**
+     * Method that controls the allocation of stats to a non-player boxer
+     * @param unusedPoints number of points the computer has available to assign to stats
+     * @return number of points spent during this step
+     */
+    private int setAIStats(int unusedPoints){
+        int pointsSpent = 0;
+        //create random stats
+        //set boxer stats
+        return pointsSpent;
     }
+
 }
