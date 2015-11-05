686cf857d67126a820af7ab5267260c672228583
Brian_Trethewey <brian.trethewey@umontana.edu>
2015-11-04 23:53:26 -0700

    Attack Builder First Pass

--- src/AttackBuilder.java
+++ src/AttackBuilder.java
@@ -5,21 +5,59 @@ import java.util.ArrayList;
  */
 //todo this class
 public class AttackBuilder {
+    // this is the list of attacks to be returned
     private ArrayList<Attack> attackList;
+    //factory for building individual attacks
+    private  AttackFactory attackFactory;
 
+    /**
+     * Constructor to create attack factory
+     */
+    public AttackBuilder() {
+        attackFactory = new AttackFactory();
+    }
+
+    /**
+     *
+     * @return Array List containing the starting attacks
+     */
     private ArrayList<Attack> buildBasicAttacks(){
-        return null;
-    }
-
-    private Attack buildSimpleAttack(String inputs){
-        return null;
-    }
-
-    public ArrayList<Attack>buildAttacks(int unusedPoints){
+        //create new attackList
+        attackList = new ArrayList<Attack>();
+        //create a basic heavy and fast attack
+        attackList.add(buildSimpleAttack("Hook|Heavy"));
+        attackList.add(buildSimpleAttack("Jab|Fast"));
+        //return for completeness
         return attackList;
     }
 
+    /**
+     *
+     * @param inputs A String with pipe delimiters, order of arguments is Attack Name, Attack Type(currently 2 types Heavy and Fast)
+     * @return Attack described by the String
+     */
+    private Attack buildSimpleAttack(String inputs){
+        return attackFactory.create(inputs);
+    }
+
+
+    /**
+     *
+     * @param unusedPoints int describing how many points player has left to use on special attacks
+     * @return the list of attacks containing the starting attacks and any special attacks the player has specified
+     */
+    public ArrayList<Attack>buildAttacks(int unusedPoints){
+        //create starting attacks
+        buildBasicAttacks();
+        return attackList;
+    }
+    /**
+     *
+     * @param unusedPoints int describing how many points player has left to use on special attacks
+     * @return the list of attacks containing the starting attacks and any special attacks the that have been randomly selected
+     */
     public ArrayList<Attack>buildAIAttacks(int unusedPoints){
+        buildBasicAttacks();
         return attackList;
     }
 }
