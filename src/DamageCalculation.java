50b44a993524aa7444e20ea97d6c66989b8e5b16
Brian_Trethewey <brian.trethewey@umontana.edu>
2015-11-05 01:16:49 -0700

    Comments to damageCalculation

--- src/DamageCalculation.java
+++ src/DamageCalculation.java
@@ -9,12 +9,25 @@ public abstract class DamageCalculation {
 
 
     //setter
+
+    /**
+     *
+     * @param successor The next Damage Calculation step
+     */
+
     public void setSuccessor(DamageCalculation successor) {
         this.successor = successor;
     }
 
 
     //abstract methods will be overwritten to preform task specified by that step
+
+    /**
+     *
+     * @param attack the Attack that is being performed
+     * @param block the Block, if any, that is being applied to the attack
+     * @return A String Detailing the results of the damage calculations, String so more than just a value can be returned if desired
+     */
     public abstract String execute(Attack attack, Block block);
 
 
