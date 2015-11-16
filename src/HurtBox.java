5dae9399aca5a6e328aa2b3c919ef8f32f456347
Brian_Trethewey <brian.trethewey@umontana.edu>
2015-11-15 23:14:08 -0700

    hurtbox calculation created

--- src/HurtBox.java
+++ src/HurtBox.java
@@ -14,8 +14,39 @@ public class HurtBox {
 
     //these variables hold the different Chain of Responsibility heads for the different linked lists for calculating damage
     private DamageCalculation heavyCalculation;
-    private DamageCalculation fastAttack;
+    private DamageCalculation fastCalculation;
     private ChanceBot chance = ChanceBot.getInstance();
+
+
+    public HurtBox(){
+        //create heavy calculation chain
+        heavyCalculation = new StrengthCalculation();
+        DamageCalculation  nextNode = new SpeedCalculation();
+        heavyCalculation.setSuccessor(nextNode);
+        DamageCalculation currentNode = nextNode;
+        nextNode = new BlockCalculation();
+        currentNode.setSuccessor(nextNode);
+        currentNode = nextNode;
+        nextNode = new AccuracyCalculation();
+        currentNode.setSuccessor(nextNode);
+        currentNode = nextNode;
+        nextNode =new FinishCalculation();
+        currentNode.setSuccessor(nextNode);
+
+        //create fast calculation chain
+        fastCalculation = new SpeedCalculation();
+        nextNode = new StrengthCalculation();
+        fastCalculation.setSuccessor(nextNode);
+        currentNode = nextNode;
+        nextNode = new BlockCalculation();
+        currentNode.setSuccessor(nextNode);
+        currentNode = nextNode;
+        nextNode = new AccuracyCalculation();
+        currentNode.setSuccessor(nextNode);
+        currentNode = nextNode;
+        nextNode =new FinishCalculation();
+        currentNode.setSuccessor(nextNode);
+    }
 
 
     /**
@@ -31,6 +62,18 @@ public class HurtBox {
         }
 
         return Integer.toString(damage);
+    }
+
+    public String calculateDamage(Attack attack,Block block, int differentiate){
+        String type = attack.getAttackType();
+        String result;
+        if (type.compareTo("Heavy")==0){
+            result =  heavyCalculation.execute(attack,block,0);
+        }else{
+            result = fastCalculation.execute(attack,block,0);
+        }
+        return result;
+
     }
 
 }
